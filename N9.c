8cdf09f8d0a5fc01a689995706e0731f1302927a
Tommaso <tommaso.corsini@iisviolamarchesini.edu.it>
2023-02-07 19:04:08 +0100

    aggiungo n9

---
+++ N9.c
@@ -0,0 +1,45 @@
+//es.9
+/*Progettare un algoritmo che risolva il seguente problema. Si richieda 
+all’utente di inserire una serie di terne di dati numerici (A, B, C). 
+Il programma deve terminare quando uno dei valori inseriti è minore di
+zero. Si scartino le terne nelle quali i valori non sono in ordine
+strettamente crescente, ovvero quelle terne per cui non valga A < B < C.
+Su tutte le terne non scartate si calcoli il massimo e il minimo dei
+valori inseriti. Si stampino a video tali valori massimi e minimi prima
+di terminare il programma.*/
+#include <stdio.h>
+#include <limits.h>
+
+int main(int argc, char *argv[])
+{
+	int a, b, c, min = max, max = 0;
+
+	do
+	{
+		printf("\ninserisci a\n");
+		scanf("%d", &a);
+		printf("inserisci b\n");
+		scanf("%d", &b);
+		printf("inserisci c\n");
+		scanf("%d", &c);
+
+		if (a < b && b < c)
+		{
+
+			if (a < min)
+				min = a;
+			if (c > max)
+				max = c;
+		}
+		else
+		{
+			printf("terna non valida\n");
+		}
+
+	} while (a >= 0 && b >= 0 && c >= 0);
+
+	printf("\nmax: %d\n", max);
+	printf("\nminimo: %d\n\n", min);
+
+	return 0;
+}
